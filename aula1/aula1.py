1c4c14c9e1c3b83b4a8a4be087c750afac6c8574
walacedelgado <walacecastro@live.com>
2024-09-08 16:09:42 -0300

    Criando Comentários

---
+++ aula1/aula1.py
@@ -0,0 +1,9 @@
+#Criando Comentários
+print("Helo World 1")
+print("Helo World 2")
+print("Helo World 3")  #Comentário de uma linha
+print("Helo World 4")
+#Comentário de uma linha
+
+"""Comentário
+Multi linha"""
