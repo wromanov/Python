da9e0b3198a9f7cbb89a3e623462b6543e50711f
walacedelgado <walacecastro@live.com>
2024-09-08 19:05:43 -0300

    Tipo string

---
+++ aula3/aula3.py
@@ -0,0 +1,14 @@
+#Tipo String
+# Uma cadeia de caracteres, tudo que estiver entre aspas ou aspas duplas em Python.
+print('Alguma Coisa')
+print("Outra Coisa")
+
+#Escapando aspas duplas ou aspas simples
+print("Essa é uma 'String' (str).")
+print('Essa é uma "String" (str).')
+
+#Caracteres de escape de string "\"
+print("Essa é uma \"String\" (str).")
+
+#utilizar o 'r" para que o interpretador execute o texto como foi escrito, sem executar nada.
+print(r"Esse é meu \n (str)")
