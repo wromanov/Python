6dfef69c35c4891719316be6994972aee9c7da1e
walacedelgado <walacecastro@live.com>
2024-09-08 19:17:31 -0300

    Tipos primitivos

---
+++ aula4/aula4.py
@@ -0,0 +1,13 @@
+#Tipos primitivos
+"""
+str - string "Cadeia de Caracter" 'Cadeia de Caracter'
+int - inteiro - 123456 0 -1
+float - ponto flutuante - 10.1 -10.1
+bool - booleano - True / False
+"""
+
+#como verificar o tipo primitivo
+print("Walace", type("Walace"))
+print(1, type(1))
+print(1.1, type(1.1))
+print(10 == 10, type(True))
