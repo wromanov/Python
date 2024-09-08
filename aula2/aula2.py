d179aa763573cbc0a222dee2490beb831c78b381
walacedelgado <walacecastro@live.com>
2024-09-08 19:05:27 -0300

    Função print

---
+++ aula2/aula2.py
@@ -0,0 +1,7 @@
+#Função Print
+
+print(123456)
+
+print("Walace", "de", "Castro", sep="#", end="")
+#A função inclui o que vai estar no final do print, por padrão envia uma quebra de linha.
+print("Walace", "Castro", sep='-')
