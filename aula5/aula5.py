1a3bb77998a506a4f71c6424cf0c0393d857a56e
Walace Delgado <walacecastro@live.com>
2024-09-09 17:45:55 -0300

    Operadores aritméticos

---
+++ aula5/aula5.py
@@ -0,0 +1,9 @@
+#Operadores Aritméticos
+
+print("Multiplicação 10 *10: ", 10 * 10)
+print("Adição 10 + 10: ", 10 + 10)
+print("Subtração 10 - 9: ", 10 - 9)
+print("Divisão Fracionada 10 / 10: ", 10 / 7)
+print("Divisão Inteira 10 // 3: ", 10 // 3)
+print("Modulo da divisão 10 % 9: ", 10 % 9)
+print("Potenciação 10 ** 2: ", 10 ** 2)
