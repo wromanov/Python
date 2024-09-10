1bec1d98e6d1d332541ba6257859de41a8c78ac8
Walace Delgado <walacecastro@live.com>
2024-09-10 17:48:36 -0300

    Atualização

--- aula9/aula9.py
+++ aula9/aula9.py
@@ -16,5 +16,7 @@ idade = int(input('Qual a sua idade? '))
 maior_idade = 60 #passou da idade
 menor_idade = 20 #muito jovem
 
-if idade >= maior_idade and idade <= menor_idade:
-    print(f"{nome} pode pegar empréstimo")
+if idade >= menor_idade and idade <= maior_idade:
+    print(f"{nome.title()} pode pegar empréstimo.")
+else:
+    print(f"{nome} não pode pegar empréstimo.")
