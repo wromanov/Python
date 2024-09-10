c304695b0e4d2460f7a92c275a41094c9ad5a791
Walace Delgado <walacecastro@live.com>
2024-09-10 17:49:21 -0300

    Função Len

---
+++ aula11/aula11.py
@@ -0,0 +1,6 @@
+#Função Len - Contar caracteres dentro e uma string.
+
+nome = input('Qual o seu nome? ')
+contar_caracteres = len(nome)
+
+print(f"O nome {nome} tem {contar_caracteres} caracteres.")
