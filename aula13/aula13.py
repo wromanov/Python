c05953f749f73b4558ebaa59c7c2e038264b9a7d
Walace Delgado <walacecastro@live.com>
2024-09-11 16:19:06 -0300

    aula13

---
+++ aula13/aula13.py
@@ -0,0 +1,12 @@
+local = True
+feixe = False
+
+if local:
+    ... #Usado para segurar um bloco para escrever posteriormente, ou podemos usar o "pass"
+else:
+    print("Aqui tem algo")
+
+if feixe:
+    pass #Usado para segurar um bloco para escrever posteriormente, ou podemos usar o "..."
+else:
+    print("Aqui tem algo")
