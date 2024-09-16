8cd12b212d59a95cf8c5e87c32c67cbcf8341e0a
Walace Delgado <walacecastro@live.com>
2024-09-16 17:19:47 -0300

    Aula 27

---
+++ aula27/aula27.py
@@ -0,0 +1,16 @@
+"""
+Operador Ternário
+"""
+
+logeed_user = False
+
+msg = "Usuário logado." if (logeed_user) else "Usuário precisa logar."
+
+print(msg)
+
+idade = 18
+e_de_maior = idade > 18
+
+msg2 = "Pode acessar." if e_de_maior else "Não pode acessar."
+
+print(msg2)
