a4ece53b85f2dbf7977cfb7940e311fcbc066694
walacedelgado <walacecastro@live.com>
2024-09-19 17:40:14 -0300

    Execício validador de CPF

--- aula30/aula30.py
+++ aula30/aula30.py
@@ -23,4 +23,39 @@ for c in range(0, 9):
     #print(soma_cpf)
 
 calculo_cpf = 11 - (soma_cpf % 11)
-print(calculo_cpf)
+
+valida_digita_1 = 0
+
+#valida o penúltimo digito do CPF
+if calculo_cpf > 9:
+    valida_digito_1 = 0
+else:
+    valida_digito_1 = calculo_cpf
+
+print(f"Calculo do CPF {calculo_cpf} Valor do Penúltimo Dígito {valida_digito_1}")
+
+#valida o último digito do CPF
+soma_cpf_2 = 0
+contagem_2 = 11
+
+for k in range(0, 10):
+    soma_cpf_2 += lista_cpf[k] * contagem_2
+    print(soma_cpf_2, lista_cpf[k], contagem_2)
+    contagem_2 -= 1  # Decrementa a contagem
+    if contagem_2 == 2:
+        soma_cpf_2 += valida_digito_1 * contagem_2  # multiplica o digito do cpf com o contador e adiciona ao acumulador
+        contagem_2 -= 1  # Decrementa a contagem
+        print(soma_cpf_2)
+        break
+
+calculo_cpf_2 = 11 - (soma_cpf_2 % 11)
+
+valida_digito_2 = 0
+
+#valida o último digito do CPF
+if calculo_cpf_2 > 9:
+    valida_digito_2 = 0
+else:
+    valida_digito_2 = calculo_cpf_2
+
+print(f"Calculo do CPF {calculo_cpf_2} Valor do Penúltimo Dígito {valida_digito_2}")
